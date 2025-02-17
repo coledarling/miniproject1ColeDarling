889bc441d78548ab757977f03d6d6a310efb917a
coledarling <t_darling@mail.fhsu.edu>
2025-02-16 20:11:54 -0600

    Final tweaks.

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
 ### INF601 - Advanced Programming in Python
 ### Cole Darling
 ### Mini Project 1
-import pprint
 import yfinance as yf
 import numpy as np
 import matplotlib.pyplot as plt
-import copy
 import os
 
 os.makedirs("charts", exist_ok=True)
 
-mytickers = ["MSFT", "AAPL", "META", "GME", "AMC"]
+myTickers = ["MSFT", "AAPL", "META", "GME", "AMC"]
 
-for ticker in mytickers:
+for ticker in myTickers:
     result = yf.Ticker(ticker)
     hist = result.history(period="10d")
     last10days = []
     for date in hist['Close']:
         last10days.append(date)
     if len(last10days) == 10:
-        #maxlist = copy.copy(last10days)
-        #maxlist.sort()
-        #max_price = maxlist[-1]+10
-        #min_price = maxlist[0]-10
         myarray = np.array(last10days)
         max_price = myarray.max() + (myarray.max()*.05)
         min_price = myarray.min() - (myarray.min()*.05)
@@ -34,8 +28,3 @@ for ticker in mytickers:
         plt.savefig(f"charts/{ticker}.png")
     else:
         print(f"Do not have 10 days of data. Only have {len(last10days)} days.")
-
-# get historical market data
-#hist = msft.history(period="1mo")
-
-#pprint.pprint(hist)
