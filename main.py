c038f692dc99cde58814908d64c2c8d0774a7594
coledarling <t_darling@mail.fhsu.edu>
2025-02-20 19:12:28 -0600

    Tried to fix too many request timeout.

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
+'''
 ### INF601 - Advanced Programming in Python
-### Cole Darling
 ### Mini Project 1
+### I,Cole Darling, affirm that the work submitted for this assignment is entirely my own. I have not engaged in any form of academic dishonesty, including but not limited to cheating, plagiarism, or the use of unauthorized materials. I have neither provided nor received unauthorized assistance and have accurately cited all sources in adherence to academic standards. I understand that failing to comply with this integrity statement may result in consequences, including disciplinary actions as determined by my course instructor and outlined in institutional policies. By signing this statement, I acknowledge my commitment to upholding the principles of academic integrity.
+'''
+
 import yfinance as yf
+from datetime import datetime, timedelta
 import numpy as np
 import matplotlib.pyplot as plt
 import os
 
 os.makedirs("charts", exist_ok=True)
 
+#Get today's date
+today = datetime.now()
+
+#Calculate the date 10 days ago
+ten_days_ago = today - timedelta(days=15)
+
 myTickers = ["MSFT", "AAPL", "META", "GME", "AMC"]
 
 for ticker in myTickers:
     result = yf.Ticker(ticker)
-    hist = result.history(period="10d")
+    hist = result.history(start=ten_days_ago, end=today)
     last10days = []
-    for date in hist['Close']:
+    for date in hist['Close'][:11]:
         last10days.append(date)
     if len(last10days) == 10:
         myarray = np.array(last10days)
@@ -26,5 +36,6 @@ for ticker in myTickers:
         plt.axis((9, 0, min_price, max_price))
         plt.title(f"{ticker} Last 10 Closing Prices")
         plt.savefig(f"charts/{ticker}.png")
+        plt.close()
     else:
         print(f"Do not have 10 days of data. Only have {len(last10days)} days.")
